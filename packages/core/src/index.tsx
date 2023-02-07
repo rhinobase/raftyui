29811b20763e6afd0b0b54055013a657946051d3
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-02-07 19:31:47 +0530

    Improved errors in Tab and Toast component and added create context file

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,3 +1,5 @@
 export { Button } from "./button";
 export { Spinner } from "./spinner";
 export { Tooltip } from "./tooltip";
+export { Toast } from "./toast";
+export { Tabs } from "./tabs";
--- packages/core/src/toast/Toast.tsx
+++ packages/core/src/toast/Toast.tsx
@@ -1,10 +1,5 @@
-import { IconType } from "react-icons";
-import {
-  HiOutlineCheckCircle,
-  HiOutlineExclamationCircle,
-  HiOutlineInformationCircle,
-} from "react-icons/hi2";
-import { classNames } from "@rhinobase/lib";
+import { classNames } from "@rhinobase/utils";
+import React from "react";
 
 export interface IToast {
   className?: string;
@@ -15,18 +10,63 @@ export interface IToast {
 }
 // Toast Component
 export function Toast(props: IToast) {
-  let ToastIcon: IconType;
+  var ToastIcon: JSX.Element;
 
   switch (props.severity) {
     case "error":
     case "warning":
-      ToastIcon = HiOutlineExclamationCircle;
+      ToastIcon = (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-6 w-6 text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+          />
+        </svg>
+      );
       break;
     case "info":
-      ToastIcon = HiOutlineInformationCircle;
+      ToastIcon = (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-6 w-6 text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+          />
+        </svg>
+      );
       break;
     case "success":
-      ToastIcon = HiOutlineCheckCircle;
+      ToastIcon = (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-6 w-6 text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      );
       break;
   }
 
@@ -40,12 +80,11 @@ export function Toast(props: IToast) {
         props.severity == "success" && "bg-success-600 dark:bg-success-400/70",
         "pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg shadow-lg",
         props.className
-      )}>
+      )}
+    >
       <div className="p-xl">
         <div className="flex items-start">
-          <div className="flex-shrink-0">
-            <ToastIcon className="h-6 w-6 text-white" aria-hidden="true" />
-          </div>
+          <div className="flex-shrink-0">{ToastIcon}</div>
           <div className="pt-sm ml-3 w-0 flex-1">
             <p className="text-sm font-medium text-white">{props.title}</p>
             <p className="mt-1 text-sm text-white">{props.message}</p>
