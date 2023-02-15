dbd42005e3255f34fa3bdd39a08dcfb577c51042
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-02-15 09:47:25 +0530

    Updated Error Message component prop type

--- packages/core/src/field/ErrorMessage.tsx
+++ packages/core/src/field/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function ErrorMessage(props: { children: JSX.Element }) {
+export function ErrorMessage(props: { children: JSX.Element | string }) {
   return (
     <p className="my-0.5 text-sm text-red-600 dark:text-red-400">
       {props.children}
