c370a89e04ebaa3c054db3802d33c48330b0bf75
r-singodiya <119913319+r-singodiya@users.noreply.github.com>
2023-02-28 15:26:05 +0530

    working on Component Testing

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -2,10 +2,17 @@ const path = require("path");
 
 module.exports = {
   stories: ["../../../packages/core/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-vite",
+  },
+  features: {
+    interactionsDebugger: true,
   },
   async viteFinal(config, { configType }) {
     // customize the Vite config here
