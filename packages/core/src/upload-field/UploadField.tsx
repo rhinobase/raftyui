0afd7af50da16bbed3a417fbed849c817e14cbd1
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-03-06 13:46:40 +0530

    Working on upload field component its still very much pending

--- packages/core/src/upload-field/UploadField.tsx
+++ packages/core/src/upload-field/UploadField.tsx
@@ -1,35 +1,64 @@
 import React from "react";
+import { Button } from "../button";
 import * as Dialog from "../dialog";
 import * as Tab from "../tabs";
 
 export function UploadField() {
   return (
-    <div className="relative rounded-md border border-dashed border-zinc-300 py-8 px-12 transition-colors dark:border-zinc-600">
-      <Dialog.Root>
+    <div className="relative rounded-md border border-dashed border-zinc-400 py-8 px-12 transition-colors dark:border-zinc-600">
+      <Dialog.Root size="lg">
         <Dialog.Trigger className="absolute top-0 left-0 z-10 h-full w-full opacity-0 outline-none" />
+        <Dialog.Overlay />
         <Dialog.Content className="!p-0">
           <Tab.Root defaultValue="Home" orientation="vertical" size="sm">
-            <Tab.List className="!bg-transparent">
+            <Tab.List className="data-[orientation=vertical]:!px-0 data-[orientation=vertical]:!py-0">
+              <div>
+                <Button
+                  colorScheme="primary"
+                  className="!my-6 mx-auto !w-max"
+                  leftIcon={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 6v12m6-6H6"
+                      />
+                    </svg>
+                  }
+                >
+                  Upload Media
+                </Button>
+              </div>
               <Tab.Trigger
                 value="Home"
-                className="data-[state=active]:!text-primary-500"
+                className="data-[state=active]:!text-primary-500 data-[orientation=vertical]:data-[state=active]:bg-primary-50 data-[orientation=vertical]:data-[state=active]:hover:bg-primary-50 data-[orientation=vertical]:hover:bg-primary-50/50 !rounded-none"
               >
                 Home
               </Tab.Trigger>
-              <div className="h-[1px] w-full bg-zinc-200" />
+              <div className="dark:bg-secondary-700 bg-secondary-200 !my-4 mx-auto h-[1px] w-4/5" />
+              <div className="text-secondary-500/90 mb-1 select-none px-3 text-xs font-bold uppercase">
+                Manage
+              </div>
               <Tab.Trigger
                 value="SiteFiles"
-                className="data-[state=active]:!text-primary-500"
+                className="data-[state=active]:!text-primary-500 data-[orientation=vertical]:data-[state=active]:bg-primary-50 data-[orientation=vertical]:data-[state=active]:hover:bg-primary-50 data-[orientation=vertical]:hover:bg-primary-50/50 !rounded-none"
               >
                 Site Files
               </Tab.Trigger>
               <Tab.Trigger
                 value="Trash"
-                className="data-[state=active]:!text-primary-500"
+                className="data-[state=active]:!text-primary-500 data-[orientation=vertical]:data-[state=active]:bg-primary-50 data-[orientation=vertical]:data-[state=active]:hover:bg-primary-50 data-[orientation=vertical]:hover:bg-primary-50/50 !rounded-none"
               >
                 Trash
               </Tab.Trigger>
-              <div className="h-[1px] w-full bg-zinc-200" />
+              <div className="dark:bg-secondary-700 bg-secondary-200 !my-4 mx-auto h-[1px] w-4/5" />
             </Tab.List>
             <div className="mx-auto w-full">
               <Tab.Content value="Home"></Tab.Content>
@@ -39,7 +68,6 @@ export function UploadField() {
           </Tab.Root>
         </Dialog.Content>
       </Dialog.Root>
-      {/* <input type="file" className="absolute z-10 h-full w-full opacity-0 outline-none" /> */}
       <div className="z-0 flex h-full w-full flex-col items-center justify-center gap-2 text-zinc-400 dark:text-zinc-600">
         <svg
           className="mx-auto h-10 w-10"
