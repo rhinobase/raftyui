2bec885ee989ca690c667938783af78865b07f89
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-03-17 15:49:19 +0530

    Made Tag Component

--- packages/core/src/tag/Tag.tsx
+++ packages/core/src/tag/Tag.tsx
@@ -3,7 +3,7 @@ import React, { forwardRef, ReactNode } from "react";
 
 export type Tag = {
   children: ReactNode;
-  className: string;
+  className?: string;
   size?: "sm" | "md" | "lg";
   colorScheme?: "primary" | "secondary" | "error" | "success" | "warning";
   leftIcon?: JSX.Element;
@@ -27,16 +27,20 @@ export const Tag = forwardRef<HTMLDivElement, Tag>(
       <div
         {...props}
         className={classNames(
-          size == "sm" && "px-1 text-sm",
-          size == "md" && "px-1 py-0.5",
-          size == "lg" && "px-2 py-1 text-lg",
-          colorScheme == "primary" && "bg-primary-500",
-          colorScheme == "secondary" && "bg-secondary-300",
-          colorScheme == "error" && "bg-error-500",
-          colorScheme == "success" && "bg-success-500",
-          colorScheme == "warning" && "bg-warning-500",
-          colorScheme != "secondary" ? "text-white" : "bg-secondary-800",
-          "rounded-sm font-medium",
+          size == "sm" && "rounded-sm px-2 text-sm",
+          size == "md" && "rounded-[4px] px-2.5",
+          size == "lg" && "rounded-md px-3 py-0.5 text-lg",
+          colorScheme == "primary" &&
+            "text-primary-500 dark:text-primary-300 bg-primary-200/30 dark:bg-primary-400/10",
+          colorScheme == "secondary" &&
+            "bg-secondary-200/60 dark:bg-secondary-700/50 dark:text-secondary-100",
+          colorScheme == "error" &&
+            "text-error-500 dark:text-error-300 bg-error-200/40 dark:bg-error-300/10",
+          colorScheme == "success" &&
+            "text-success-600 dark:text-success-300 bg-success-200/40 dark:bg-success-300/10",
+          colorScheme == "warning" &&
+            "text-warning-600 dark:text-warning-300 bg-warning-200/40 dark:bg-warning-300/10",
+          "h-max w-max font-medium",
           className,
         )}
         ref={forwardedRef}
