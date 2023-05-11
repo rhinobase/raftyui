d4ca6dad129112f32708ebf3dfd717e1f0566f4b
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-05-11 17:27:14 +0530

    Added Toggle Group Component

---
+++ libs/components/toggle-group/src/lib/context.ts
@@ -0,0 +1,13 @@
+import { createContext } from '@rhino/utils';
+
+export const [ToggleGroupProvider, useToggleGroupContext] =
+  createContext<ToggleGroupContext>({
+    name: 'ToggleGroupContext',
+    hookName: 'useToggleGroupContext',
+    providerName: '<ToggleGroup />',
+  });
+
+export interface ToggleGroupContext {
+  size: 'sm' | 'md' | 'lg';
+  barebone: boolean;
+}
