62a2b482999f1d46319292d4004eacf77b441621
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-05-16 15:39:09 +0530

    Working on upload field

--- libs/components/upload/src/lib/upload.tsx
+++ libs/components/upload/src/lib/upload.tsx
@@ -3,7 +3,9 @@ import { Progress } from "@rhino/progress";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { classNames } from "@rhino/utils";
 
-export function Upload({ size = "md" }: { size?: "sm" | "md" | "lg" }) {
+export type Upload = { size?: "sm" | "md" | "lg" };
+
+export function Upload({ size = "md" }: Upload) {
   return (
     <>
       <div className="flex w-full justify-center cursor-pointer rounded-lg border border-dashed hover:border-primary-500 border-gray-900/25 transition-all duration-500">
@@ -12,8 +14,8 @@ export function Upload({ size = "md" }: { size?: "sm" | "md" | "lg" }) {
           name="file-upload"
           type="file"
           className="sr-only"
-          onChange={(e) => {
-            var output;
+          onChange={(event) => {
+            let output;
             output.src = URL.createObjectURL(event.target.files[0]);
             output.onload = function () {
               URL.revokeObjectURL(output.src); // free memory
@@ -42,18 +44,18 @@ export function Upload({ size = "md" }: { size?: "sm" | "md" | "lg" }) {
               aria-hidden="true"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                clip-rule="evenodd"
-              ></path>
+                clipRule="evenodd"
+              />
             </svg>
             <div
               className={classNames(
                 size === "sm" && "text-sm",
-                "mt-4 flex-col justify-center text-base leading-7 text-gray-400"
+                "mt-2 flex-col justify-center text-base leading-7 text-gray-400"
               )}
             >
-              <p>Upload a file</p>
+              Upload a file
               <p
                 className={classNames(
                   size === "sm" && "hidden",
@@ -72,11 +74,22 @@ export function Upload({ size = "md" }: { size?: "sm" | "md" | "lg" }) {
       <div
         className={classNames(
           size === "sm" && "px-1",
-          "flex w-full mt-2 gap-1 items-center justify-between"
+          "flex w-full mt-1 gap-1 items-center justify-between"
         )}
       >
-        <Progress size="sm" value={50} />
-        <Button variant="ghost" size="icon">
+        <Progress
+          size="sm"
+          minValue={0}
+          maxValue={100}
+          value={25}
+          className="rounded-sm"
+        />
+        <Button
+          colorScheme="error"
+          variant="ghost"
+          size="sm"
+          className="!text-secondary-400 hover:!text-error-500 !p-[1px]"
+        >
           <TrashIcon className="h-4 w-4" />
         </Button>
       </div>
