032bd2612fd5b864f71dd337b103fd891d0556a3
r-singodiya <119913319+r-singodiya@users.noreply.github.com>
2023-05-17 17:31:12 +0530

    Working on Upload Field

--- libs/components/upload/src/lib/upload.tsx
+++ libs/components/upload/src/lib/upload.tsx
@@ -1,47 +1,84 @@
 import { Button } from "@rhino/button";
 import { Progress } from "@rhino/progress";
-import { TrashIcon } from "@heroicons/react/24/outline";
+import { PhotoIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { classNames } from "@rhino/utils";
+import { useDropzone } from "react-dropzone";
+import { forwardRef, useEffect, useState } from "react";
 
-export type Upload = { size?: "sm" | "md" | "lg" };
+type UploadFile = {
+  name?: string;
+  percent?: number;
+  status?: "error" | "warning" | "success" | "primary";
+  thumbUrl?: string;
+  uid?: string;
+  url?: string;
+};
 
-export function Upload({ size = "md" }: Upload) {
-  return (
-    <>
-      <div className="flex w-full justify-center cursor-pointer rounded-lg border border-dashed hover:border-primary-500 border-gray-900/25 transition-all duration-500">
-        <input
-          id="file-upload"
-          name="file-upload"
-          type="file"
-          className="sr-only"
-        />
-        <label
-          htmlFor="file-upload"
-          className={classNames(
-            size === "sm" && "py-8",
-            size === "md" && "py-14",
-            size === "lg" && "py-14",
-            "relative rounded-md w-full cursor-pointer bg-white font-semibold focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-          )}
+export type Upload = Omit<JSX.IntrinsicElements["input"], "size"> & {
+  size?: "sm" | "md" | "lg";
+  previewFile?: boolean;
+  action?: string;
+  customRequest?: (file: string) => Promise<string> | string;
+  data?: (file: File) => object | object | Promise<object>;
+  defaultFileList?: object[];
+  fileList?: UploadFile[];
+  onChange?: (filelist: File[]) => void;
+  onPreview?: (file: File) => void;
+  onDrop?: (event: React.DragEvent) => void;
+  onRemove?: (file: File) => void | Promise<boolean>;
+  progress?: boolean;
+  progressValue?: number;
+};
+
+export const Upload = forwardRef<HTMLInputElement, Upload>(
+  (
+    {
+      progressValue = 15,
+      size = "md",
+      previewFile = true,
+      progress = true,
+      ...props
+    },
+    forwardedRef
+  ) => {
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const [files, setFiles] = useState<File[]>();
+    useEffect(() => {
+      setFiles(acceptedFiles);
+    }, [acceptedFiles]);
+
+    console.log(acceptedFiles);
+    return (
+      <>
+        <div
+          {...getRootProps()}
+          className="flex w-full justify-center cursor-pointer rounded-lg border border-dashed hover:border-primary-500 border-gray-900/25 transition-all duration-500"
         >
-          <div className="text-center">
-            <svg
+          <input
+            {...props}
+            name="file-upload"
+            type="file"
+            onChange={(e) => ""}
+            {...getInputProps()}
+            ref={forwardedRef}
+          />
+          <label
+            htmlFor="file-upload"
+            className={classNames(
+              size === "sm" && "py-8",
+              size === "md" && "py-14",
+              size === "lg" && "py-14",
+              "relative rounded-md w-full text-center cursor-pointer bg-white font-semibold focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+            )}
+          >
+            <PhotoIcon
               className={classNames(
                 size === "sm" && "h-6 w-6",
                 size === "md" && "h-8 w-8",
                 size === "lg" && "h-12 w-12",
-                "mx-auto text-gray-300 h-12 w-12"
+                "mx-auto text-gray-400 h-12 w-12"
               )}
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                clipRule="evenodd"
-              />
-            </svg>
+            />
             <div
               className={classNames(
                 size === "sm" && "text-sm",
@@ -61,31 +98,67 @@ export function Upload({ size = "md" }: Upload) {
                 uploading company data or other banned files.
               </p>
             </div>
-          </div>
-        </label>
-      </div>
-      <div
-        className={classNames(
-          size === "sm" && "px-1",
-          "flex w-full mt-1 gap-1 items-center justify-between"
-        )}
-      >
-        <Progress
-          size="sm"
-          minValue={0}
-          maxValue={100}
-          value={25}
-          className="rounded-sm"
-        />
-        <Button
-          colorScheme="error"
-          variant="ghost"
-          size="sm"
-          className="!text-secondary-400 hover:!text-error-500 !p-[1px]"
+          </label>
+        </div>
+        <div
+          className={classNames(
+            size === "sm" && "px-1",
+            "flex w-full mt-1 gap-1 items-center justify-between"
+          )}
         >
-          <TrashIcon className="h-4 w-4" />
-        </Button>
-      </div>
-    </>
-  );
-}
+          {progress && (
+            <>
+              <Progress
+                size="sm"
+                minValue={0}
+                maxValue={100}
+                value={progressValue}
+                className="rounded-sm"
+              />
+              <Button
+                colorScheme="error"
+                variant="ghost"
+                size="sm"
+                className="!text-secondary-400 hover:!text-error-500 !p-[1px]"
+              >
+                <TrashIcon className="h-4 w-4" />
+              </Button>
+            </>
+          )}
+        </div>
+        {previewFile && (
+          <div className="w-full overflow-x-auto">
+            <div className="flex gap-2 items-center w-max">
+              {files &&
+                files.map((file, index) => (
+                  <div
+                    className="relative h-28 w-28 group overflow-hidden border rounded-md p-2"
+                    key={index + file.name}
+                  >
+                    <img
+                      src={URL.createObjectURL(file)}
+                      alt={file.name}
+                      className="h-28 w-28 object-cover"
+                    />
+                    <div className="absolute top-0 left-0 w-full h-full gap-2 items-center justify-center hidden group-hover:flex bg-black/60 transition-all">
+                      <Button
+                        size="icon"
+                        variant="solid"
+                        colorScheme="error"
+                        onClick={() => {
+                          acceptedFiles.splice(index, 1);
+                          setFiles([...acceptedFiles]);
+                        }}
+                      >
+                        <TrashIcon className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
+);
