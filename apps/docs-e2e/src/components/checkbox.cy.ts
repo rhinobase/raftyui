3c333abb331749f2437fa36fb336bf761a612c7a
dbundel <123345732+dbundel@users.noreply.github.com>
2023-05-19 12:04:19 +0530

    test: working on checkbox test

--- apps/docs-e2e/src/components/checkbox.cy.ts
+++ apps/docs-e2e/src/components/checkbox.cy.ts
@@ -3,5 +3,18 @@ describe("Drawer Test", () => {
     cy.visit("/iframe.html?args=&id=form-checkbox--default&viewMode=story")
   );
 
-  it("Basic Test for CheckBox", () => {});
+  it("Basic Test for CheckBox", () => {
+    // Test for checkbox
+    cy.get('[aria-checked="false"]')
+      .first()
+      .should("have.attr", "aria-checked", "false");
+    cy.get('[aria-checked="false"]').first().click();
+    cy.should("have.attr", "aria-checked", "true");
+    // Test for indeterminate checkbox
+    cy.get("h2")
+      .contains("CheckBox indeterminate")
+      .next()
+      .should("have.attr", "aria-disabled", "false");
+    cy.get("h2").contains("CheckBox indeterminate").next().click();
+  });
 });
