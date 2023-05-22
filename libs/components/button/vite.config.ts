810bb0636ce401164ac09e71d9a1c98afad78bdf
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2023-05-22 07:54:49 +0530

    chore: minor fix

--- libs/components/button/vite.config.ts
+++ libs/components/button/vite.config.ts
@@ -7,7 +7,6 @@ import { join } from "path";
 
 export default defineConfig({
   cacheDir: "../../../node_modules/.vite/components-button",
-
   plugins: [
     dts({
       entryRoot: "src",
@@ -32,14 +31,14 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    target: "esnext",
+    minify: "terser",
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: "src/index.ts",
-      name: "components-button",
+      name: "button",
       fileName: "index",
-      // Change this to the formats you want to support.
-      // Don't forgot to update your package.json as well.
-      formats: ["es", "cjs"],
+      formats: ["es", "umd", "iife"],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
--- libs/core/vite.config.ts
+++ libs/core/vite.config.ts
@@ -32,14 +32,12 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    target: "esnext",
     lib: {
-      // Could also be a dictionary or array of multiple entry points.
       entry: "src/index.ts",
       name: "core",
       fileName: "index",
-      // Change this to the formats you want to support.
-      // Don't forgot to update your package.json as well.
-      formats: ["es", "cjs"],
+      formats: ["es", "umd", "iife"],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
