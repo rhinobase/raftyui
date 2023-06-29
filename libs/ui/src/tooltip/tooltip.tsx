5b96ca12dfc8ad2a6434fec5af66d386f14439ba
r-singodiya <119913319+r-singodiya@users.noreply.github.com>
2023-06-29 18:39:09 +0530

    docs: add tooltips

--- libs/ui/src/tooltip/tooltip.tsx
+++ libs/ui/src/tooltip/tooltip.tsx
@@ -1,67 +1,69 @@
-import React from "react";
+import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { classNames } from "@rafty/utils";
 
-export type Tooltip = {
-  children: React.ReactNode;
-  content: React.ReactNode;
-  isArrow?: boolean;
-  delayDuration?: number;
-  hasAnimation?: boolean;
-  open?: boolean;
-  defaultOpen?: boolean;
-  side?: "top" | "right" | "bottom" | "left";
-  align?: "start" | "center" | "end";
-  sideOffset?: number;
-  alignOffset?: number;
-  onOpenChange?: (open: boolean) => void;
-  className?: string;
-};
-export function Tooltip({
-  children,
-  className,
-  content,
-  isArrow = true,
-  delayDuration = 50,
-  hasAnimation = true,
-  open,
-  defaultOpen,
-  onOpenChange,
-  side = "top",
-  align = "center",
-  sideOffset = 10,
-  alignOffset,
-}: Tooltip) {
-  return (
-    <TooltipPrimitive.Provider>
-      <TooltipPrimitive.Root
-        delayDuration={delayDuration}
-        open={open}
-        defaultOpen={defaultOpen}
-        onOpenChange={onOpenChange}
-      >
-        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
-        <TooltipPrimitive.Portal>
-          <TooltipPrimitive.Content
-            side={side}
-            align={align}
-            sideOffset={sideOffset}
-            alignOffset={alignOffset}
-            className={classNames(
-              hasAnimation === true &&
-                "data-[side=top]:animate-slide-down-fade data-[side=right]:animate-slide-left-fade data-[side=bottom]:animate-slide-up-fade data-[side=left]:animate-slide-right-fade",
-              typeof content == "string" &&
-                "bg-secondary-800 text-secondary-100 dark:bg-secondary-100 dark:text-secondary-700 relative z-40 max-w-[250px] rounded-md px-2 py-1 text-xs font-medium shadow-md",
-              className
-            )}
-          >
-            {content}
-            {isArrow && (
-              <TooltipPrimitive.Arrow className="fill-secondary-800 dark:fill-secondary-50" />
-            )}
-          </TooltipPrimitive.Content>
-        </TooltipPrimitive.Portal>
-      </TooltipPrimitive.Root>
-    </TooltipPrimitive.Provider>
-  );
-}
+const TooltipProvider = TooltipPrimitive.Provider;
+
+export type Tooltip = React.ComponentPropsWithoutRef<
+  typeof TooltipPrimitive.Root
+>;
+
+export const Tooltip = ({ children, ...props }: Tooltip) => (
+  <TooltipProvider>
+    <TooltipPrimitive.Root {...props}>{children}</TooltipPrimitive.Root>
+  </TooltipProvider>
+);
+Tooltip.displayName = "Tooltip";
+
+export type TooltipTrigger = React.ComponentPropsWithoutRef<
+  typeof TooltipPrimitive.Trigger
+>;
+
+export const TooltipTrigger = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Trigger>,
+  TooltipTrigger
+>(({ className, children, ...props }, forwardedRef) => (
+  <TooltipPrimitive.Trigger ref={forwardedRef} {...props} asChild>
+    {children}
+  </TooltipPrimitive.Trigger>
+));
+TooltipTrigger.displayName = "TooltipTrigger";
+
+export type TooltipContent = React.ComponentPropsWithoutRef<
+  typeof TooltipPrimitive.Content
+> & { isArrow?: boolean; hasAnimation?: boolean };
+
+export const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  TooltipContent
+>(
+  (
+    {
+      children,
+      className,
+      hasAnimation = true,
+      sideOffset = 4,
+      isArrow = true,
+      ...props
+    },
+    forwardedRef
+  ) => (
+    <TooltipPrimitive.Content
+      ref={forwardedRef}
+      sideOffset={sideOffset}
+      className={classNames(
+        hasAnimation &&
+          "data-[side=top]:animate-slide-down-fade data-[side=right]:animate-slide-left-fade data-[side=bottom]:animate-slide-up-fade data-[side=left]:animate-slide-right-fade",
+        "bg-secondary-800 text-secondary-100 dark:bg-secondary-100 dark:text-secondary-700 relative z-40 max-w-[250px] rounded-md px-2 py-1 text-xs font-medium shadow-md",
+        className
+      )}
+      {...props}
+    >
+      {children}
+      {isArrow && (
+        <TooltipPrimitive.Arrow className="fill-secondary-800 dark:fill-secondary-50" />
+      )}
+    </TooltipPrimitive.Content>
+  )
+);
+TooltipContent.displayName = "TooltipContent";
