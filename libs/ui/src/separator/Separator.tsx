cccfaec9e8d8737b7b2f5864afb5908c4737c940
dbundel <123345732+dbundel@users.noreply.github.com>
2023-06-29 19:16:56 +0530

    docs: added seperator component

---
+++ libs/ui/src/separator/Separator.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import SeparatorPrimitive from "@radix-ui/react-separator";
+import { classNames } from "@rafty/utils";
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={classNames(
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
+
+export { Separator };
