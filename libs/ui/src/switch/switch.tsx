698823a3beb7fdb1217d84b7f0796266fb42fd4a
r-singodiya <119913319+r-singodiya@users.noreply.github.com>
2023-06-29 18:56:07 +0530

    docs: added Switch

--- libs/ui/src/switch/switch.tsx
+++ libs/ui/src/switch/switch.tsx
@@ -70,7 +70,6 @@
 import * as React from "react";
 import * as SwitchPrimitives from "@radix-ui/react-switch";
 import { classNames } from "@rafty/utils";
-import { SwitchAria } from "react-aria";
 
 const switchClasses = {
   size: {
@@ -82,9 +81,9 @@ const switchClasses = {
 
 const switchThumbClasses = {
   size: {
-    sm: "h-3 w-3 data-[state=checked]:translate-x-4",
-    md: "h-5 w-5 data-[state=checked]:translate-x-5",
-    lg: "h-6 w-6 data-[state=checked]:translate-x-6",
+    sm: "h-3 w-3 data-[state=checked]:translate-x-3",
+    md: "h-5 w-5 data-[state=checked]:translate-x-4",
+    lg: "h-6 w-6 data-[state=checked]:translate-x-5",
   },
 };
 
@@ -94,14 +93,14 @@ export type Switch = React.ComponentPropsWithoutRef<
   size?: "sm" | "md" | "lg";
 };
 
-const Switch = React.forwardRef<
+export const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   Switch
 >(({ className, size = "md", ...props }, ref) => (
   <SwitchPrimitives.Root
     className={classNames(
       switchClasses.size[size],
-      "peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-secondary-100 data-[state=unchecked]:bg-input",
+      "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-300 data-[state=unchecked]:bg-secondary-400 dark:data-[state=unchecked]:bg-secondary-600",
       className
     )}
     {...props}
@@ -109,12 +108,10 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={classNames(
-        switchClasses.size[size],
-        "pointer-events-none block rounded-full data-[state=checked]:bg-primary-500 shadow-lg ring-0 transition-transform  data-[state=unchecked]:translate-x-0"
+        switchThumbClasses.size[size],
+        "pointer-events-none block rounded-full bg-secondary-100 dark:data-[state=checked]:bg-secondary-900 data-[state=checked]:bg-secondary-100 shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0"
       )}
     />
   </SwitchPrimitives.Root>
 ));
-Switch.displayName = SwitchPrimitives.Root.displayName;
-
-export { Switch };
+Switch.displayName = "Switch";
