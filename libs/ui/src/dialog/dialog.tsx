8f7e0586a72aca0f8da494e9b06d4768a88117c0
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-07-10 14:01:27 +0530

    chore: minor improvements in dialog content's default close button

--- libs/ui/src/dialog/dialog.tsx
+++ libs/ui/src/dialog/dialog.tsx
@@ -167,8 +167,8 @@ export const DialogContent = React.forwardRef<
         >
           {children}
           {showCloseButton && (
-            <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-              <XMarkIcon className="h-4 w-4 stroke-2" />
+            <DialogPrimitive.Close className="absolute right-4 top-4 rounded p-1 transition-all hover:bg-secondary-200/70 dark:hover:bg-secondary-500/70 focus:outline-none">
+              <XMarkIcon className="h-4 w-4 stroke-2 " />
             </DialogPrimitive.Close>
           )}
         </DialogPrimitive.Content>
