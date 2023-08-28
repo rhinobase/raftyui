ad353f2a698fc0ec1a1549dd933f4d70765d5bff
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-08-28 15:38:42 +0530

    fix: took out the value prop from switch as was creating issue when used in forms

--- libs/ui/src/switch/switch.tsx
+++ libs/ui/src/switch/switch.tsx
@@ -19,8 +19,9 @@ const switchThumbClasses = {
   },
 };
 // Switch component
-export type Switch = React.ComponentPropsWithoutRef<
-  typeof SwitchPrimitives.Root
+export type Switch = Omit<
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>,
+  "value"
 > & {
   size?: "sm" | "md" | "lg";
 };
@@ -28,22 +29,28 @@ export type Switch = React.ComponentPropsWithoutRef<
 export const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   Switch
->(({ className, size = "md", ...props }, forwardedref) => (
-  <SwitchPrimitives.Root
-    className={classNames(
-      switchClasses.size[size],
-      "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-300 data-[state=unchecked]:bg-secondary-400 dark:data-[state=unchecked]:bg-secondary-600",
-      className,
-    )}
-    {...props}
-    ref={forwardedref}
-  >
-    <SwitchPrimitives.Thumb
+>(
+  (
+    { className, size = "md", defaultChecked = false, ...props },
+    forwardedref,
+  ) => (
+    <SwitchPrimitives.Root
+      {...props}
+      defaultChecked={defaultChecked}
       className={classNames(
-        switchThumbClasses.size[size],
-        "pointer-events-none block rounded-full bg-secondary-100 dark:data-[state=checked]:bg-secondary-900 data-[state=checked]:bg-secondary-100 shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0",
+        switchClasses.size[size],
+        "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-300 data-[state=unchecked]:bg-secondary-400 dark:data-[state=unchecked]:bg-secondary-600",
+        className,
       )}
-    />
-  </SwitchPrimitives.Root>
-));
+      ref={forwardedref}
+    >
+      <SwitchPrimitives.Thumb
+        className={classNames(
+          switchThumbClasses.size[size],
+          "pointer-events-none block rounded-full bg-secondary-100 dark:data-[state=checked]:bg-secondary-900 data-[state=checked]:bg-secondary-100 shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0",
+        )}
+      />
+    </SwitchPrimitives.Root>
+  ),
+);
 Switch.displayName = "Switch";
