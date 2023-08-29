b35a5b35a90341802164e9f28d7344935b1315da
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-08-29 12:46:20 +0530

    fix: added field context props to checkbox and switch components

--- libs/ui/src/checkbox/checkbox.tsx
+++ libs/ui/src/checkbox/checkbox.tsx
@@ -7,9 +7,6 @@ import { classNames } from "../utils";
 import { useFieldControlContext } from "../field/context";
 
 // Checkbox Component
-export type Checkbox = React.ComponentPropsWithoutRef<
-  typeof CheckboxPrimitive.Root
-> & { size?: "sm" | "md" | "lg" };
 
 const CheckBoxClasses = {
   size: { sm: "h-4 w-4", md: "h-5 w-5", lg: "h-6 w-6" },
@@ -18,22 +15,45 @@ const CheckBoxIndicatorClasses = {
   size: { sm: "h-2.5 w-2.5", md: "h-3.5 w-3.5", lg: "h-4 w-4" },
 };
 
+export type Checkbox = Omit<
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>,
+  "disabled" | "required"
+> & {
+  size?: "sm" | "md" | "lg";
+  isReadOnly?: boolean;
+  isDisabled?: boolean;
+  isRequired?: boolean;
+};
+
 export const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   Checkbox
 >(
   (
-    { className, children, size = "md", name, required, disabled, ...props },
+    {
+      className,
+      children,
+      size = "md",
+      isReadOnly = false,
+      isDisabled = false,
+      isRequired = false,
+      ...props
+    },
     forwardedref,
   ) => {
     const context = useFieldControlContext() ?? {};
 
+    const name = props.name || context.name,
+      disabled =
+        isDisabled || context.isDisabled || isReadOnly || context.isReadOnly,
+      required = isRequired || context.isRequired;
+
     const checkbox = (
       <CheckboxPrimitive.Root
         {...props}
-        name={name ?? context.name}
-        required={required ?? context.isRequired}
-        disabled={disabled ?? context.isDisabled}
+        name={name}
+        disabled={disabled}
+        required={required}
         className={classNames(
           CheckBoxClasses.size[size],
           "relative shrink-0 rounded-sm border border-secondary-400 dark:border-secondary-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-500 data-[state=checked]:border-primary-500 dark:data-[state=checked]:bg-primary-300 dark:data-[state=checked]:border-primary-300 ",
--- libs/ui/src/switch/switch.tsx
+++ libs/ui/src/switch/switch.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import * as SwitchPrimitives from "@radix-ui/react-switch";
 import { classNames } from "../utils";
+import { useFieldControlContext } from "../field/context";
 
 const switchClasses = {
   size: {
@@ -21,9 +22,12 @@ const switchThumbClasses = {
 // Switch component
 export type Switch = Omit<
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>,
-  "value"
+  "value" | "disabled" | "required"
 > & {
   size?: "sm" | "md" | "lg";
+  isReadOnly?: boolean;
+  isDisabled?: boolean;
+  isRequired?: boolean;
 };
 
 export const Switch = React.forwardRef<
@@ -31,26 +35,46 @@ export const Switch = React.forwardRef<
   Switch
 >(
   (
-    { className, size = "md", defaultChecked = false, ...props },
-    forwardedref,
-  ) => (
-    <SwitchPrimitives.Root
-      {...props}
-      defaultChecked={defaultChecked}
-      className={classNames(
-        switchClasses.size[size],
-        "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-300 data-[state=unchecked]:bg-secondary-400 dark:data-[state=unchecked]:bg-secondary-600",
-        className,
-      )}
-      ref={forwardedref}
-    >
-      <SwitchPrimitives.Thumb
+    {
+      className,
+      size = "md",
+      defaultChecked = false,
+      isReadOnly = false,
+      isDisabled = false,
+      isRequired = false,
+      ...props
+    },
+    forwardedRef,
+  ) => {
+    const context = useFieldControlContext() ?? {};
+
+    const name = props.name || context.name,
+      disabled =
+        isDisabled || context.isDisabled || isReadOnly || context.isReadOnly,
+      required = isRequired || context.isRequired;
+
+    return (
+      <SwitchPrimitives.Root
+        {...props}
+        defaultChecked={defaultChecked}
+        name={name}
+        disabled={disabled}
+        required={required}
         className={classNames(
-          switchThumbClasses.size[size],
-          "pointer-events-none block rounded-full bg-secondary-100 dark:data-[state=checked]:bg-secondary-900 data-[state=checked]:bg-secondary-100 shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0",
+          switchClasses.size[size],
+          "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-300 data-[state=unchecked]:bg-secondary-400 dark:data-[state=unchecked]:bg-secondary-600",
+          className,
         )}
-      />
-    </SwitchPrimitives.Root>
-  ),
+        ref={forwardedRef}
+      >
+        <SwitchPrimitives.Thumb
+          className={classNames(
+            switchThumbClasses.size[size],
+            "pointer-events-none block rounded-full bg-secondary-100 dark:data-[state=checked]:bg-secondary-900 data-[state=checked]:bg-secondary-100 shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0",
+          )}
+        />
+      </SwitchPrimitives.Root>
+    );
+  },
 );
 Switch.displayName = "Switch";
