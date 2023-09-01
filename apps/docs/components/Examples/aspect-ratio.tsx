f4621b5a02434b3ed7fa7a4e6604e2744aead720
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2023-09-01 11:24:06 +0530

    docs: small changes

--- apps/docs/components/Examples/aspect-ratio.tsx
+++ apps/docs/components/Examples/aspect-ratio.tsx
@@ -1,14 +1,12 @@
 import { AspectRatio } from "@rafty/ui";
+
 export const aspect_ratio_examples = {
   "aspect-ratio:usage": (
-    <div className="shadow-blackA7 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]">
-      <AspectRatio ratio={16 / 9}>
-        <img
-          className="h-full w-full object-cover"
-          src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
-          alt="Landscape photograph by Tobias Tullius"
-        />
-      </AspectRatio>
-    </div>
+    <AspectRatio ratio={3 / 4} className="overflow-hidden">
+      <img
+        src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
+        alt="Landscape photograph by Tobias Tullius"
+      />
+    </AspectRatio>
   ),
 };
--- apps/docs/components/Examples/avatar.tsx
+++ apps/docs/components/Examples/avatar.tsx
@@ -1,10 +1,10 @@
 import { Avatar, AvatarGroup } from "@rafty/ui";
 
 export const avatar_examples = {
-  "avatar:usage": <Avatar name="Ganjendra" />,
+  "avatar:usage": <Avatar name="Sample Name" />,
   "avatar:size": (
     <Avatar
-      name="image"
+      name="Jhon"
       src="https://images.unsplash.com/photo-1682695795255-b236b1f1267d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       size="sm"
     />
@@ -12,19 +12,19 @@ export const avatar_examples = {
   "avatar:avatar-group": (
     <AvatarGroup max={2} size="lg" className="h-12">
       <Avatar
-        name="image"
+        name="Lily"
         src="https://images.unsplash.com/photo-1682695795255-b236b1f1267d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       />
       <Avatar
-        name="photo"
+        name="Jhon"
         src="https://images.unsplash.com/photo-1682695795255-b236b1f1267d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       />
       <Avatar
-        name="profile image"
+        name="Aman"
         src="https://images.unsplash.com/photo-1682695795255-b236b1f1267d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       />
       <Avatar
-        name="user image"
+        name="Jhon"
         src="https://images.unsplash.com/photo-1682695795255-b236b1f1267d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       />
     </AvatarGroup>
