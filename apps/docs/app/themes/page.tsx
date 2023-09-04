d39ebc965762afc6a5a90be07a789bf4a586f5ef
dinesh mali <126282539+Dk579@users.noreply.github.com>
2023-09-04 18:11:01 +0530

    docs: make selectable button functionality

--- apps/docs/app/themes/page.tsx
+++ apps/docs/app/themes/page.tsx
@@ -1,16 +1,21 @@
 "use client";
+import React, { useState } from "react";
 import {
   Button,
   Calendar,
   DatePicker,
   InputField,
   PasswordField,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
   RangePicker,
   Tag,
   TagField,
   Textarea,
 } from "@rafty/ui";
-import { useState } from "react";
+import { BsSun, BsMoon } from "react-icons/bs";
+import { HiCheck } from "react-icons/hi2";
 import Masonry from "react-responsive-masonry";
 
 enum ColorTheme {
@@ -20,34 +25,95 @@ enum ColorTheme {
 
 export default function Home() {
   const [theme, setTheme] = useState<ColorTheme>();
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
+
+  const THEMES = [
+    "zinc",
+    "slate",
+    "stone",
+    "rose",
+    "orange",
+    "blue",
+    "yellow",
+    "green",
+    "red",
+  ];
+  const handleColorSelect = (color: string) => {
+    setSelectedColor(color);
+  };
 
   return (
-    <div className={`max-w-7xl mx-auto p-4 w-full `} id="demotheme">
-      <h1 className="text-4xl font-semibold">
-        Beautifully designed components built using Rafty UI and Tailwind CSS
-      </h1>
-      <div className="p-3 flex items-center gap-4 justify-end">
-        Themes
-        <Button
-          size="icon"
-          className="!bg-orange-500"
-          onClick={() => setTheme(ColorTheme.ORANGE)}
-        />
-        <Button
-          size="icon"
-          className="!bg-green-500"
-          onClick={() => setTheme(ColorTheme.GREEN)}
-        />
-        <Button
-          size="icon"
-          className="!bg-blue-500"
-          onClick={() => setTheme(ColorTheme.ORANGE)}
-        />
-        <Button
-          size="icon"
-          className="!bg-red-500"
-          onClick={() => setTheme(ColorTheme.ORANGE)}
-        />
+    <div className="p-10 w-full">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-5xl font-bold">Make it yours.</h1>
+          <h4 className="text-secondary-400 text-lg">
+            Hand-picked themes that you can copy and paste into your apps.
+          </h4>
+        </div>
+        <div className="p-3 flex items-center gap-4 justify-end">
+          <Button
+            size="icon"
+            className="!bg-orange-500 "
+            onClick={() => setTheme(ColorTheme.ORANGE)}
+          />
+          <Button
+            size="icon"
+            className="!bg-green-500"
+            onClick={() => setTheme(ColorTheme.GREEN)}
+          />
+          <Button
+            size="icon"
+            className="!bg-blue-500"
+            onClick={() => setTheme(ColorTheme.ORANGE)}
+          />
+          <Button
+            size="icon"
+            className="!bg-red-500"
+            onClick={() => setTheme(ColorTheme.ORANGE)}
+          />
+          <Popover>
+            <PopoverTrigger>Customize</PopoverTrigger>
+            <PopoverContent>
+              <div className="w-[300px] p-2 space-y-2">
+                <h5 className="text-xs">Color</h5>
+                <div className="grid grid-cols-3 gap-2">
+                  {THEMES.map((color) => (
+                    <Button
+                      key={color}
+                      size="sm"
+                      variant="outline"
+                      leftIcon={
+                        <div
+                          className={`h-4 w-4 bg-${color}-500 rounded-full items-center`}
+                        >
+                          {selectedColor === color && (
+                            <HiCheck
+                              className="text-white font-bold"
+                              size={15}
+                            />
+                          )}
+                        </div>
+                      }
+                      onClick={() => handleColorSelect(color)}
+                    >
+                      {color.charAt(0).toUpperCase() + color.slice(1)}
+                    </Button>
+                  ))}
+                </div>
+                <h5 className="text-xs">Mode</h5>
+                <div className="flex gap-4">
+                  <Button variant="outline" size="sm" leftIcon={<BsSun />}>
+                    Light
+                  </Button>
+                  <Button variant="outline" size="sm" leftIcon={<BsMoon />}>
+                    Dark
+                  </Button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
       <Masonry columnsCount={3} gutter="70px" className="mt-10">
         <DatePicker />
