1e2fac832948846debfae4048cde04dde92abf69
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-09-05 17:25:25 +0530

    feat: added hashcode to rgb code coverter script

---
+++ script.js
@@ -0,0 +1,36 @@
+// Hex to RGB Code converter
+const axios = require("axios");
+const colors = require("tailwindcss/colors");
+
+async function main() {
+  const items = [
+    "purple",
+    "teal",
+    "fuchsia",
+    "rose",
+    "pink",
+    "indigo",
+    "blue",
+    "sky",
+    "emerald",
+  ];
+
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    console.log(`.theme-${item} {`);
+    await Promise.all(
+      Object.entries(colors[item]).map(([w, c]) =>
+        axios.get(`https://www.thecolorapi.com/id?hex=${c.slice(1)}`).then(
+          ({
+            data: {
+              rgb: { r, g, b },
+            },
+          }) => console.log(`--color-primary-${w}: ${r} ${g} ${b};`),
+        ),
+      ),
+    );
+    console.log("}");
+  }
+}
+
+main();
