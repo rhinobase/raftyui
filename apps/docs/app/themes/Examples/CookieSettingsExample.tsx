a92d958813338c5cdc2065cdb049ae4feea798db
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-09-08 13:56:20 +0530

    docs: working on theme examples review

--- apps/docs/app/themes/Examples/CookieSettingsExample.tsx
+++ apps/docs/app/themes/Examples/CookieSettingsExample.tsx
@@ -19,25 +19,29 @@ export function CookieSettingsExample() {
     },
   ];
   return (
-    <>
-      <Text className="text-xl font-semibold">Cookie Settings</Text>
-      <Text className="text-sm opacity-60 leading-snug">
-        Manage your cookie settings here.
-      </Text>
+    <div className="space-y-4">
+      <div>
+        <Text className="text-xl font-semibold leading-snug">
+          Cookie Settings
+        </Text>
+        <Text className="text-sm opacity-60">
+          Manage your cookie settings here.
+        </Text>
+      </div>
       {Content.map((item, index) => (
-        <div key={index} className="flex gap-4 py-3 items-center">
+        <div key={index} className="flex justify-between items-center">
           <div>
-            <Text className="text-sm">{item.heading}</Text>
-            <Text className="text-xs text-secondary-500 dark:text-secondary-400">
+            <Text className="text-sm font-medium">{item.heading}</Text>
+            <Text className="text-xs opacity-50 max-w-[250px]">
               {item.discription}
             </Text>
           </div>
           <Switch defaultChecked={index == 2 ? false : true} />
         </div>
       ))}
-      <Button variant="outline" colorScheme="primary" className="!w-full">
+      <Button variant="outline" colorScheme="primary" className="!w-full !mt-5">
         Save preference
       </Button>
-    </>
+    </div>
   );
 }
