d4707b739468500ae6b73bfc6d6fa607595db16f
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-11-09 12:12:21 +0530

    docs(custom): improved drawer styling in small screen

--- apps/docs/components/MobileNavigation.tsx
+++ apps/docs/components/MobileNavigation.tsx
@@ -1,8 +1,8 @@
 import { Dispatch, SetStateAction } from "react";
 import Link from "next/link";
 import { Drawer, DrawerClose, DrawerContent, DrawerOverlay } from "@rafty/ui";
-import { Logomark } from "../components/Logo";
-import { Navigation } from "../components/Navigation";
+import { Logomark } from "./Logo";
+import { Navigation } from "./Navigation";
 
 export type MobileNavigation = {
   isOpen: boolean;
@@ -28,14 +28,17 @@ export function MobileNavigation({ isOpen, setOpen }: MobileNavigation) {
       side="left"
     >
       <DrawerOverlay />
-      <DrawerContent className="!w-full !max-w-[70vw] !p-0">
-        <div className="dark:bg-secondary-800 sticky top-0 z-[60] bg-white p-6">
+      <DrawerContent className="!w-full !max-w-[70vw] !p-0 !h-[100dvh] !overflow-hidden flex flex-col">
+        <div className="dark:bg-secondary-800 bg-white p-6">
           <Link href="/" aria-label="Home page">
             <Logomark className="h-9 w-9" />
           </Link>
           <DrawerClose />
         </div>
-        <Navigation className="h-full px-6 pb-6" onLinkClick={onLinkClick} />
+        <Navigation
+          className="flex-1 px-6 pb-6 overflow-y-auto"
+          onLinkClick={onLinkClick}
+        />
       </DrawerContent>
     </Drawer>
   );
