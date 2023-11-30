e9d936d179b4118d829338c75c815948b5bcc365
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2023-11-30 12:41:43 +0530

    feat(web): added link in PrevNextLink

--- apps/docs/components/PrevNextLinks.tsx
+++ apps/docs/components/PrevNextLinks.tsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { classNames } from "@rafty/ui";
 import { navigation } from "../lib/navigation";
-import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  PencilIcon,
+} from "@heroicons/react/24/outline";
 
 function PageLink({
   title,
@@ -52,9 +56,33 @@ export function PrevNextLinks() {
   }
 
   return (
-    <dl className="border-secondary-200 dark:border-secondary-800 mt-12 flex border-t pt-6">
-      {previousPage && <PageLink dir="previous" {...previousPage} />}
-      {nextPage && <PageLink className="ml-auto text-right" {...nextPage} />}
+    <dl className="border-secondary-200 dark:border-secondary-800 mt-12 flex flex-col border-t">
+      <EditOnGithub pathname={pathname} />
+      <div className="flex mt-6">
+        {previousPage && <PageLink dir="previous" {...previousPage} />}
+        {nextPage && <PageLink className="ml-auto text-right" {...nextPage} />}
+      </div>
     </dl>
+  );
+}
+
+function EditOnGithub({ pathname }: { pathname: string }) {
+  // Link to .md file
+  const href = [
+    "https://github.com/rhinobase/raftyui/blob/main/apps/docs/app",
+    pathname,
+    "/page.md",
+  ].join("");
+
+  return (
+    <a
+      href={href}
+      className="py-4 md:py-6 w-max flex gap-2 items-center text-secondary-500 dark:text-secondary-400 hover:text-blue-500 dark:hover:text-blue-300 transition-all ease-in-out"
+      target="_blank"
+      referrerPolicy="no-referrer"
+    >
+      <PencilIcon height={15} width={15} className="stroke-2" />
+      <p className="text-sm">Edit this page on GitHub</p>
+    </a>
   );
 }
