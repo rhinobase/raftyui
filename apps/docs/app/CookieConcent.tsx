8280816b959b998d408d8c0d42c5379a90239260
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2023-12-01 15:15:10 +0530

    fix(web): imporoved CookieConcent

--- apps/docs/app/CookieConcent.tsx
+++ apps/docs/app/CookieConcent.tsx
@@ -1,11 +1,24 @@
+"use client";
 import { Button, Text } from "@rafty/ui";
 import Script from "next/script";
+import cookies from "js-cookie";
+import { useState } from "react";
 
-// export const CONCENT_COOKIE_NAME = "rccn";
+type CookieConcent = {
+  defaultValue?: string;
+  cookieKey: string;
+};
 
-export function CookieConcent() {
-  // TODO: Fix this rendering
-  const isConcentGiven = true; // cookies[CONCENT_COOKIE_NAME];
+export function CookieConcent({ defaultValue, cookieKey }: CookieConcent) {
+  const [isConcentGiven, setConcent] = useState(defaultValue);
+
+  function handleGivenConcent(accept = true) {
+    const token = String(Number(accept));
+    return () => {
+      cookies.set(cookieKey, token);
+      setConcent(token);
+    };
+  }
 
   // Adding google analytics
   if (isConcentGiven)
@@ -27,15 +40,26 @@ export function CookieConcent() {
       </>
     );
   // Asking permission
-  else
+  else if (isConcentGiven == undefined)
     return (
-      <div className="fixed z-50 bottom-0 bg-white dark:bg-secondary-900 w-full shadow-[0_-3px_10px_0px_rgba(0,0,0,0.1)] dark:shadow-none">
-        <div className="flex flex-col md:flex-row md:items-center justify-between max-w-4xl mx-auto px-3 py-3 md:px-0 md:py-4 gap-4">
-          <Text className="text-sm opacity-60 max-w-xl leading-snug text-center md:text-left">
+      <div className="dark:bg-secondary-900 fixed bottom-0 z-50 w-full bg-white shadow-[0_-3px_10px_0px_rgba(0,0,0,0.1)] dark:shadow-none">
+        <div className="mx-auto flex max-w-4xl flex-col justify-between gap-4 px-3 py-3 md:flex-row md:items-center md:px-0 md:py-4">
+          <Text className="max-w-xl text-center text-sm leading-snug opacity-60 md:text-left">
             We use cookies to enhance your user experience. By using our
             webiste, you agree to our use of cookies.
           </Text>
-          <Button colorScheme="primary">Got it!</Button>
+          <div className="flex gap-3">
+            <Button
+              colorScheme="primary"
+              size="sm"
+              onClick={handleGivenConcent()}
+            >
+              Accept
+            </Button>
+            <Button size="sm" onClick={handleGivenConcent(false)}>
+              Opt out
+            </Button>
+          </div>
         </div>
       </div>
     );
