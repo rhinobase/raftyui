32ef1c05a9f403720047aba2907470222538d81a
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2023-12-01 15:55:16 +0530

    style(web): made responsive CookieConsent component

--- apps/docs/app/CookieConsent.tsx
+++ apps/docs/app/CookieConsent.tsx
@@ -28,20 +28,25 @@ export function CookieConsent({ defaultValue, cookieKey }: CookieConsent) {
   else if (isConsentGiven == undefined)
     return (
       <div className="dark:bg-secondary-900 fixed bottom-0 z-50 w-full bg-white shadow-[0_-3px_10px_0px_rgba(0,0,0,0.1)] dark:shadow-none">
-        <div className="mx-auto flex max-w-4xl flex-col justify-between gap-4 px-3 py-3 md:flex-row md:items-center md:px-0 md:py-4">
+        <div className="mx-auto flex max-w-4xl flex-col items-center justify-between gap-4 px-3 py-3 md:flex-row md:items-center md:py-4 lg:px-0">
           <Text className="max-w-xl text-center text-sm leading-snug opacity-60 md:text-left">
             We only collect analytics essential to ensuring smooth operation of
             our services.
           </Text>
-          <div className="flex gap-3">
+          <div className="flex w-full gap-3 md:w-auto">
             <Button
               colorScheme="primary"
               size="sm"
+              className="!w-full md:!w-auto"
               onClick={handleGivenConsent()}
             >
               Accept
             </Button>
-            <Button size="sm" onClick={handleGivenConsent(false)}>
+            <Button
+              size="sm"
+              className="!w-full md:!w-auto"
+              onClick={handleGivenConsent(false)}
+            >
               Opt out
             </Button>
           </div>
