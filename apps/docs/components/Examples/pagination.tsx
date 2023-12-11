5926ce36c6bd58465055afd50f8af559783b2862
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2023-12-11 17:05:10 +0530

    chore(web): review changes in docs

--- apps/docs/components/Examples/pagination.tsx
+++ apps/docs/components/Examples/pagination.tsx
@@ -4,41 +4,23 @@ export const pagination_examples = {
   "pagination:usage": (
     <Pagination
       total={100}
-      pageSize={10}
       showTotal={(total) => `Total Data ${total}`}
       className="justify-between"
     />
   ),
-  "pagination:show_size_changer": (
-    <Pagination total={100} pageSize={10} showSizeChanger />
-  ),
+  "pagination:show_size_changer": <Pagination total={100} showSizeChanger />,
 
-  "pagination:show_quick_jumper": (
-    <Pagination total={100} showQuickJumper pageSize={10} />
-  ),
+  "pagination:show_quick_jumper": <Pagination total={100} showQuickJumper />,
   "pagination:size": (
-    <Pagination
-      total={100}
-      pageSize={10}
-      size="sm"
-      showQuickJumper
-      showSizeChanger
-    />
+    <Pagination total={100} size="sm" showQuickJumper showSizeChanger />
   ),
   "pagination:Total_Number": (
     <Pagination
       total={100}
-      pageSize={10}
       showTotal={(total, range) => `${range} of ${total} items`}
     />
   ),
   "pagination:is_disabled": (
-    <Pagination
-      total={100}
-      isDisabled
-      showQuickJumper
-      pageSize={10}
-      showSizeChanger
-    />
+    <Pagination total={100} isDisabled showQuickJumper showSizeChanger />
   ),
 };
