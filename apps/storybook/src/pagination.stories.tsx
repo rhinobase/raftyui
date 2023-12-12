ed7edc657538750b2326f1abc537f8dd727fed97
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2023-12-12 10:04:36 +0530

    docs(storybook): improved pagination storybook

--- apps/storybook/src/pagination.stories.tsx
+++ apps/storybook/src/pagination.stories.tsx
@@ -4,8 +4,13 @@ import { useState } from "react";
 
 const meta: Meta<typeof Pagination> = {
   title: "Components / Pagination",
+  args: {
+    size: "md",
+    isDisabled: false,
+  },
   argTypes: {
     size: {
+      control: "select",
       options: ["sm", "md", "lg"],
     },
   },
@@ -15,25 +20,85 @@ export default meta;
 type Story = StoryObj<typeof Pagination>;
 
 export const Default: Story = {
-  render: function Render({ size }) {
+  render: ({ size, isDisabled }) => (
+    <div className="flex h-full items-center justify-center">
+      <Pagination
+        total={100}
+        pageSizeOptions={[10, 20, 30]}
+        hideOnSinglePage
+        defaultPageSize={20}
+        showQuickJumper
+        isDisabled={isDisabled}
+        showSizeChanger
+        size={size}
+        showTotal={(total, range) => `Total ${total} items, Range is ${range}`}
+        className="flex gap-5"
+      />
+    </div>
+  ),
+};
+
+export const Goto: Story = {
+  render: ({ size, isDisabled }) => (
+    <div className="flex h-full items-center justify-center">
+      <Pagination
+        total={100}
+        pageSizeOptions={[10, 20, 30]}
+        showQuickJumper
+        size={size}
+        isDisabled={isDisabled}
+        className="flex gap-5"
+      />
+    </div>
+  ),
+};
+
+export const SizeChanger: Story = {
+  render: ({ size, isDisabled }) => (
+    <div className="flex h-full items-center justify-center">
+      <Pagination
+        total={100}
+        pageSizeOptions={[10, 20, 30]}
+        showSizeChanger
+        size={size}
+        isDisabled={isDisabled}
+        className="flex gap-5"
+      />
+    </div>
+  ),
+};
+export const Buttons: Story = {
+  render: ({ size, isDisabled }) => (
+    <div className="flex h-full items-center justify-center">
+      <Pagination
+        total={100}
+        pageSizeOptions={[10, 20, 30]}
+        size={size}
+        isDisabled={isDisabled}
+        className="flex gap-5"
+      />
+    </div>
+  ),
+};
+
+export const ControledPageNumber: Story = {
+  render: function Render({ size, isDisabled }) {
     const [current, setCurrent] = useState(3);
 
     const onChange = (page: number) => {
       setCurrent(page);
     };
+
     return (
       <div className="flex h-full items-center justify-center">
         <Pagination
           total={100}
           pageSizeOptions={[10, 20, 30]}
           hideOnSinglePage
-          defaultPageSize={20}
           showQuickJumper
           showSizeChanger
           size={size}
-          showTotal={(total, range) =>
-            `Total ${total} items, Range is ${range}`
-          }
+          isDisabled={isDisabled}
           current={current}
           onChange={onChange}
           className="flex gap-5"
@@ -43,40 +108,15 @@ export const Default: Story = {
   },
 };
 
-export const Goto: Story = {
-  render: ({ size }) => (
-    <div className="flex h-full items-center justify-center">
-      <Pagination
-        total={100}
-        pageSizeOptions={[10, 20, 30]}
-        showQuickJumper
-        size={size}
-        className="flex gap-5"
-      />
-    </div>
-  ),
-};
-
-export const SizeChanger: Story = {
-  render: ({ size }) => (
-    <div className="flex h-full items-center justify-center">
-      <Pagination
-        total={100}
-        pageSizeOptions={[10, 20, 30]}
-        showSizeChanger
-        size={size}
-        className="flex gap-5"
-      />
-    </div>
-  ),
-};
-export const Buttons: Story = {
-  render: ({ size }) => (
+export const TotalNumber: Story = {
+  render: ({ size, isDisabled }) => (
     <div className="flex h-full items-center justify-center">
       <Pagination
         total={100}
         pageSizeOptions={[10, 20, 30]}
         size={size}
+        showTotal={(total, range) => `Total ${total} items, Range is ${range}`}
+        isDisabled={isDisabled}
         className="flex gap-5"
       />
     </div>
