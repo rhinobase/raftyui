d20430b9c16ae49e6c3b76caf06787eb65422081
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2023-12-12 16:34:44 +0530

    chore: minor changes

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -13,7 +13,3 @@ const config: StorybookConfig = {
 };
 
 export default config;
-
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
--- libs/ui/src/accordion/index.ts
+++ libs/ui/src/accordion/index.ts
@@ -6,3 +6,4 @@ export {
   accordionContentClasses,
   accordionTriggerClasses,
 } from "./Accordion";
+export { useAccordionContext } from "./context";
