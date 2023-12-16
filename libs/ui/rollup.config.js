4d616780a13d52183b5f70b33a8fb54263ab062c
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2023-12-16 10:31:43 +0530

    fix(ui): improved compiler

--- libs/ui/rollup.config.js
+++ libs/ui/rollup.config.js
@@ -1,6 +1,5 @@
 const preserveDirectives = require("rollup-plugin-preserve-directives");
 const nxConfig = require("@nx/react/plugins/bundle-rollup");
-const terser = require("@rollup/plugin-terser");
 
 module.exports = (_config) => {
   const config = nxConfig(_config);
@@ -14,7 +13,6 @@ module.exports = (_config) => {
     plugins: [
       ...config.plugins,
       preserveDirectives.default(), // For preserving "use client" directives
-      terser(), // For minifing bundle
     ],
   };
 };
