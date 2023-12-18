85de0f022f20118ba10ebe7067c7ed28caf73dd8
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2023-12-18 17:27:56 +0530

    fix(ui): improved ErrorMessage component rendering

--- libs/ui/src/error-message/ErrorMessage.tsx
+++ libs/ui/src/error-message/ErrorMessage.tsx
@@ -5,17 +5,21 @@ import { classNames } from "../utils";
 export type ErrorMessage = HTMLAttributes<HTMLParagraphElement>;
 
 export const ErrorMessage = forwardRef<HTMLParagraphElement, ErrorMessage>(
-  ({ className, ...props }, forwardedRef) => {
-    return (
-      <p
-        {...props}
-        className={classNames(
-          "my-0.5 text-sm text-red-600 dark:text-red-400",
-          className,
-        )}
-        ref={forwardedRef}
-      />
-    );
+  ({ children, className, ...props }, forwardedRef) => {
+    if (children)
+      return (
+        <p
+          {...props}
+          className={classNames(
+            "my-0.5 text-sm text-red-600 dark:text-red-400",
+            className,
+          )}
+          ref={forwardedRef}
+        >
+          {children}
+        </p>
+      );
+    return undefined;
   },
 );
 ErrorMessage.displayName = "ErrorMessage";
