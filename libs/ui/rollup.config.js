fe76e496bb8294518593cdf3ccd6e6d750894e3f
Aditya Mathur <mathuraditya724@gmail.com>
2024-01-03 12:17:00 +0530

    chore: config changes

--- libs/ui/rollup.config.js
+++ libs/ui/rollup.config.js
@@ -1,5 +1,4 @@
 const preserveDirectives = require("rollup-plugin-preserve-directives");
-const nxConfig = require("@nx/react/plugins/bundle-rollup");
 const terser = require("@rollup/plugin-terser");
 const { visualizer } = require("rollup-plugin-visualizer");
 
@@ -8,15 +7,15 @@ const getFiles = require("../../scripts/get-files");
 const extensions = [".js", ".ts", ".jsx", ".tsx"];
 const excludeExtensions = [".stories.tsx"];
 
-module.exports = (_config) => {
-  const config = nxConfig(_config);
-
+module.exports = (config) => {
   config.preserveModules = true;
+
   config.plugins.push(
     preserveDirectives.default(), // For preserving "use client" directives
     visualizer(), // For visualizing bundle size
     terser(), // For minification
   );
+
   config.input = [
     config.input.index,
     ...getFiles("libs/ui/src", extensions, excludeExtensions),
