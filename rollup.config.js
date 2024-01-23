d07ebdaa056017ce30fad88a43b5b30a9a85ee0c
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-01-24 00:25:32 +0530

    chore: removed .esm from the bundles name

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,12 @@ const { visualizer } = require("rollup-plugin-visualizer");
 module.exports = (config) => {
   config.preserveModules = true;
 
+  config.output = {
+    ...config.output,
+    entryFileNames: "[name].js",
+    chunkFileNames: "[name].js",
+  };
+
   config.plugins.push(
     preserveDirectives.default(), // For preserving "use client" directives
     visualizer(), // For visualizing bundle size
