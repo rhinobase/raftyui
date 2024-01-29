560a57507412446d4039f5e0de42f028a3421057
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-01-29 11:42:49 +0530

    fix(web): updated tailwind config

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
   darkMode: "class",
   content: [
     "{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,md,html}",
-    "../../libs/ui/src/**/*!(*.stories|*.spec).{ts,tsx}",
-    "libs/corp/src/**/*!(*.stories|*.spec).{ts,tsx}",
-    "libs/fibr/src/**/*!(*.stories|*.spec).{ts,tsx}",
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
