2612e040b7db5f6ab51c1f8dce8cce0acb770209
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-02-08 11:59:28 +0530

    chore(ui): minor improvement in tooltip

--- libs/ui/src/tooltip/Tooltip.tsx
+++ libs/ui/src/tooltip/Tooltip.tsx
@@ -2,12 +2,15 @@ import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { BooleanOrFunction, getValue } from "@rafty/shared";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import { classNames } from "../utils";
-import { TooltipProvider, useTooltipContext } from "./context";
+import {
+  type TooltipContext,
+  TooltipProvider,
+  useTooltipContext,
+} from "./context";
 
 // Tooltip Component
-export type Tooltip = ComponentPropsWithoutRef<typeof TooltipPrimitive.Root> & {
-  isDisabled?: boolean;
-};
+export type Tooltip = ComponentPropsWithoutRef<typeof TooltipPrimitive.Root> &
+  Partial<TooltipContext>;
 
 export const Tooltip = ({
   children,
@@ -65,25 +68,26 @@ export const TooltipContent = forwardRef<
     const _hasAnimation = getValue(hasAnimation) ?? true;
     const { isDisabled } = useTooltipContext();
 
-    if (!isDisabled)
-      return (
-        <TooltipPrimitive.Content
-          ref={forwardedRef}
-          sideOffset={sideOffset}
-          className={classNames(
-            "bg-secondary-800 text-secondary-100 dark:bg-secondary-100 dark:text-secondary-700 relative z-40 max-w-[250px] rounded-md px-2 py-1 text-xs font-medium shadow-md",
-            _hasAnimation &&
-              "data-[side=top]:animate-slide-down-fade data-[side=right]:animate-slide-left-fade data-[side=bottom]:animate-slide-up-fade data-[side=left]:animate-slide-right-fade",
-            className,
-          )}
-          {...props}
-        >
-          {children}
-          {_isArrow && (
-            <TooltipPrimitive.Arrow className="fill-secondary-800 dark:fill-secondary-50" />
-          )}
-        </TooltipPrimitive.Content>
-      );
+    if (isDisabled) return;
+
+    return (
+      <TooltipPrimitive.Content
+        ref={forwardedRef}
+        sideOffset={sideOffset}
+        className={classNames(
+          "bg-secondary-800 text-secondary-100 dark:bg-secondary-100 dark:text-secondary-700 relative z-40 max-w-[250px] rounded-md px-2 py-1 text-xs font-medium shadow-md",
+          _hasAnimation &&
+            "data-[side=top]:animate-slide-down-fade data-[side=right]:animate-slide-left-fade data-[side=bottom]:animate-slide-up-fade data-[side=left]:animate-slide-right-fade",
+          className,
+        )}
+        {...props}
+      >
+        {children}
+        {_isArrow && (
+          <TooltipPrimitive.Arrow className="fill-secondary-800 dark:fill-secondary-50" />
+        )}
+      </TooltipPrimitive.Content>
+    );
   },
 );
 TooltipContent.displayName = "TooltipContent";
