f365686092f2550189c6aa7f7b3cc80dd2785605
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-05-14 16:05:49 +0530

    docs(storybook): updated date picker story

--- packages/ui/src/date-picker/date-picker.stories.tsx
+++ packages/ui/src/date-picker/date-picker.stories.tsx
@@ -1,28 +1,28 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import dateFormat from "dateformat";
 import { DatePicker } from "./DatePicker";
 
 const meta: Meta<typeof DatePicker> = {
   title: "Form / DatePicker",
-  args: {
-    startOfWeek: 1,
-    positioning: {
-      placement: "top-start",
-    },
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof DatePicker>;
 
 export const Default: Story = {
-  render: (props) => <DatePicker {...props} />,
+  render: (props) => (
+    <DatePicker
+      {...props}
+      format={(date) => `${date.day}/${date.month}/${date.year}`}
+    />
+  ),
 };
 
 export const DefaultValue: Story = {
   render: () => {
     const date = new Date();
-    // const currentDate = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+    const currentDate = dateFormat(date, "isoDate");
 
-    return <DatePicker defaultValue={[date.toISOString().split("T")[0]]} />;
+    return <DatePicker defaultValue={[currentDate]} />;
   },
 };
