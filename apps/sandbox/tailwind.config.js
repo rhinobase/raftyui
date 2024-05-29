2d481aae43a9b488a1d4b1d78d946c462818c640
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-05-29 14:56:14 +0530

    fix(storybook): corrected tailwindcss config

--- apps/sandbox/tailwind.config.js
+++ apps/sandbox/tailwind.config.js
@@ -1,10 +1,7 @@
-const { createGlobPatternsForDependencies } = require("@nx/react/tailwind");
-const { join } = require("node:path");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["apps/docs/.storybook/preview.tsx", "packages/**/*.{tsx,ts}"],
+  content: ["apps/sandbox/.storybook/preview.tsx", "packages/**/*.{tsx,ts}"],
   theme: {
     extend: {},
   },
