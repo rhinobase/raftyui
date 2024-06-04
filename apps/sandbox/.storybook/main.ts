4335c2cd5e45ade3c85673c05000e2a5568a23f5
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-06-04 06:49:03 +0200

    chore(sandbox): minor changes

--- apps/sandbox/.storybook/main.ts
+++ apps/sandbox/.storybook/main.ts
@@ -3,11 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../../../packages/ui/src/**/*.stories.tsx",
-    "../../../packages/icons/src/**/*.stories.tsx",
-    "../../../packages/blocks/src/**/*.stories.tsx",
-  ],
+  stories: ["../../../packages/**/*.stories.tsx"],
   addons: ["@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-vite",
@@ -28,6 +24,10 @@ const config: StorybookConfig = {
           "@rafty/ui": path.resolve(
             __dirname,
             "../../../packages/ui/src/index.ts",
+          ),
+          "@rafty/corp": path.resolve(
+            __dirname,
+            "../../../packages/corp/src/index.ts",
           ),
           "@rafty/icons": path.resolve(
             __dirname,
