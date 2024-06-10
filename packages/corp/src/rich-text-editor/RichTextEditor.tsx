980d42efd58373275368305a9ba03a33dd3b1abc
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-06-10 11:51:37 +0530

    chore(corp): improved rich text editor component

--- packages/corp/src/rich-text-editor/RichTextEditor.tsx
+++ packages/corp/src/rich-text-editor/RichTextEditor.tsx
@@ -7,60 +7,79 @@ import EditorJS, {
   type ToolSettings,
 } from "@editorjs/editorjs";
 import { type ValueOrFunction, classNames, getValue } from "@rafty/ui";
-import { type HtmlHTMLAttributes, useEffect, useState } from "react";
+import { cva } from "class-variance-authority";
+import { type HTMLAttributes, useEffect } from "react";
 import { EDITOR_JS_TOOLS } from "./EditorConstants";
 import "./style.css";
+
+const richTextEditorClasses = cva(
+  "h-[400px] w-full overflow-hidden overflow-y-auto rounded-md border px-3 py-2",
+  {
+    variants: {
+      invalid: {
+        true: "border-red-500 dark:border-red-300",
+        false: "border-secondary-300 dark:border-secondary-700",
+      },
+      hidden: {
+        true: "hidden",
+        false: "",
+      },
+    },
+  },
+);
 
 export type RichTextEditor = {
   onChange?: (
     api: API,
     event: BlockMutationEvent | BlockMutationEvent[],
   ) => void;
-  className?: HtmlHTMLAttributes<HTMLDivElement>["className"];
+  className?: HTMLAttributes<HTMLDivElement>["className"];
   isReadOnly?: ValueOrFunction;
   data?: OutputData;
   placeholder?: string;
   tools?: { [toolName: string]: ToolConstructable | ToolSettings };
+  isHidden?: ValueOrFunction;
+  isInvalid?: ValueOrFunction;
+  id?: string;
 };
 
 export function RichTextEditor({
   onChange,
   className,
-  isReadOnly,
+  isReadOnly = false,
   data,
   tools,
+  isHidden = false,
+  isInvalid = false,
+  id = "editorjs",
   placeholder = "Type Something...",
 }: RichTextEditor) {
   const readOnly = getValue(isReadOnly);
-  let initialized = false;
+  const hidden = getValue(isHidden);
+  const invalid = getValue(isInvalid);
 
-  const [editor, setEditor] = useState<EditorJS>();
+  const editor = new EditorJS({
+    placeholder,
+    holder: id,
+    tools: { ...EDITOR_JS_TOOLS, ...tools },
+    readOnly,
+    data,
+    onChange,
+    autofocus: true,
+  });
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
-    if (!initialized) {
-      initialized = true;
-      setEditor(
-        new EditorJS({
-          placeholder,
-          holder: "editorjs",
-          tools: { ...EDITOR_JS_TOOLS, ...tools },
-          readOnly,
-          data,
-          onChange,
-        }),
-      );
-    }
-  }, []);
+    editor.readOnly?.toggle(readOnly);
+  }, [readOnly]);
 
   return (
     <div
+      id={id}
       className={classNames(
-        "border-secondary-300 dark:border-secondary-700 h-[400px] w-full overflow-hidden overflow-y-auto rounded-md border px-3 py-2 ",
+        richTextEditorClasses({ hidden, invalid }),
         className,
       )}
-    >
-      <div id="editorjs" />
-    </div>
+    />
   );
 }
--- packages/corp/src/rich-text-editor/rich-text-editor.stories.tsx
+++ packages/corp/src/rich-text-editor/rich-text-editor.stories.tsx
@@ -3,11 +3,16 @@ import { RichTextEditor } from "./RichTextEditor";
 
 const meta: Meta<typeof RichTextEditor> = {
   title: "Corp / RichTextEditor",
+  args: {
+    isReadOnly: false,
+    isHidden: false,
+    isInvalid: false,
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof RichTextEditor>;
 
 export const Default: Story = {
-  render: () => <RichTextEditor />,
+  render: (props) => <RichTextEditor {...props} />,
 };
