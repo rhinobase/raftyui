edff539931e5bc3fc2975c987f257c7cc6b9eca9
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-06-20 15:11:23 +0530

    chore(ui): updated the chat box component

--- packages/corp/src/chat-box/ChatBox.tsx
+++ packages/corp/src/chat-box/ChatBox.tsx
@@ -1,213 +1,104 @@
 "use client";
-import {
-  ArrowDownTrayIcon,
-  DocumentDuplicateIcon,
-  PaperAirplaneIcon,
-  TrashIcon,
-} from "@heroicons/react/24/outline";
-import {
-  Avatar,
-  Button,
-  InputField,
-  InputGroup,
-  Suffix,
-  Text,
-  eventHandler,
-} from "@rafty/ui";
+import { DocumentDuplicateIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { Avatar, Button, Text, classNames } from "@rafty/ui";
 import { useCopyToClipboard } from "@uidotdev/usehooks";
-import { useReducer, useState } from "react";
 
-type DataType = { id: string; message: string; by: string };
-
-export function ChatBox() {
-  const [items, dispatch] = useReducer(
-    (prev: DataType[], cur: DataType | string | null) => {
-      if (cur == null) return [];
-      if (typeof cur === "string") {
-        const index = prev.findIndex((item) => item.id === cur);
-        if (index > -1) {
-          prev.splice(index, 1);
-        }
-      } else {
-        prev.push(cur);
-      }
-      return [...prev];
-    },
-    [],
-  );
-
-  const onDownload = () => {
-    // Convert the data array into a CSV string
-    const csvString = [
-      ["message", "by"], // Specify your headers here
-      ...items.map((item) => [item.message, item.by]), // Map your data fields accordingly
-    ]
-      .map((row) => row.join(","))
-      .join("\n");
-
-    // Create a Blob from the CSV string
-    const blob = new Blob([csvString], { type: "text/csv" });
-
-    // Generate a download link and initiate the download
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "download.csv";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
-  };
-
-  const deleteMessage = () => dispatch(null);
-
-  const hasHeaderButtonDisabled = items.length === 0;
-
-  return (
-    <div className="dark:border-secondary-700 border-secondary-300 flex h-[500px] w-full flex-col rounded-lg border">
-      <ChatBoxHeader
-        onDelete={deleteMessage}
-        onDownload={onDownload}
-        isDisabled={hasHeaderButtonDisabled}
-      />
-      <div className="flex-1 overflow-hidden overflow-y-auto px-3.5 py-2.5">
-        {items.length === 0 ? (
-          <NoDataFound />
-        ) : (
-          <div className="space-y-3">
-            {items.map((chat) => (
-              <ChatBoxCard
-                key={chat.id}
-                chat={chat}
-                onMessageDelete={dispatch}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-      <MessageSend
-        onMessageSent={(message) => {
-          let id = "0";
-          if (items.length > 0) {
-            const index = items.length - 1;
-            id = String(Number(items[index].id) - 1);
-          }
-          dispatch({ id, by: "me", message });
-        }}
-      />
-    </div>
-  );
+export enum SENDER {
+  user = 0,
+  other = 1,
 }
 
-type ChatBoxHeader = {
-  onDelete: () => void;
-  onDownload: () => void;
-  isDisabled: boolean;
+export type ChatType = {
+  id: string;
+  message: string;
+  by: string;
+  sender: SENDER;
+  time: string;
 };
 
-function ChatBoxHeader({ onDelete, onDownload, isDisabled }: ChatBoxHeader) {
+export type ChatBox = {
+  data: ChatType[];
+};
+
+export function ChatBox({ data }: ChatBox) {
   return (
-    <div className="dark:border-secondary-700 border-secondary-300 flex items-center justify-between border-b px-3.5 py-2.5">
-      <h2 className="text-lg font-semibold">Chat</h2>
-      <div className="flex items-center gap-2">
-        <Button
-          variant="ghost"
-          size="icon"
-          isDisabled={isDisabled}
-          onClick={onDownload}
-        >
-          <ArrowDownTrayIcon className="size-4 stroke-2" />
-        </Button>
-        <Button
-          variant="ghost"
-          colorScheme="error"
-          size="icon"
-          isDisabled={isDisabled}
-          onClick={onDelete}
-        >
-          <TrashIcon className="size-4 stroke-2" />
-        </Button>
-      </div>
+    <div className="flex-1 overflow-hidden overflow-y-auto px-3.5 pt-2.5">
+      {data.length === 0 ? (
+        <NoDataFound />
+      ) : (
+        <div className="space-y-3">
+          {data.map((chat) => (
+            <ChatBoxCard key={chat.id} chat={chat} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 type ChatBoxCard = {
-  chat: DataType;
-  onMessageDelete: (id: string) => void;
+  chat: ChatType;
 };
 
-function ChatBoxCard({ chat, onMessageDelete }: ChatBoxCard) {
+function ChatBoxCard({ chat }: ChatBoxCard) {
   const [, copyToClipboard] = useCopyToClipboard();
-
-  const handleMessageDelete = eventHandler(() => onMessageDelete(chat.id));
-
-  return (
-    <div className="group flex items-center gap-3">
-      <Avatar name={chat.by} size="sm" />
-      <div className="space-y-1">
-        <p className="text-sm font-medium capitalize">{chat.by}</p>
-        <p className="text-xs">{chat.message}</p>
-      </div>
-      <div className="flex-1" />
-      <Button
-        size="icon"
-        variant="ghost"
-        title="copy"
-        className="invisible group-hover:visible"
-        onClick={() => copyToClipboard(chat.message)}
-      >
-        <DocumentDuplicateIcon className="size-3.5" />
-      </Button>
-      <Button
-        size="icon"
-        variant="ghost"
-        className="invisible group-hover:visible"
-        onClick={handleMessageDelete}
-        onKeyDown={handleMessageDelete}
-      >
-        <TrashIcon className="size-3.5" />
-      </Button>
-    </div>
-  );
-}
-
-type MessageSend = {
-  onMessageSent: (value: string) => void;
-};
-
-function MessageSend({ onMessageSent }: MessageSend) {
-  const [value, setValue] = useState("");
-
-  const handleMessageSend = eventHandler(() => {
-    onMessageSent(value);
-    setValue("");
-  });
+  const isUser = chat.sender === SENDER.user;
 
   return (
-    <div className="flex items-center gap-2 px-3.5 pb-3.5 pt-1.5">
-      <InputGroup>
-        <InputField
-          placeholder="Type a message..."
-          value={value}
-          className="leading-[1.5rem] placeholder:text-[0.875rem]"
-          onChange={(e) => setValue(e.target.value)}
-          onKeyDown={handleMessageSend}
-        />
-        <Suffix className="pointer-events-auto">
-          <Button
-            size="icon"
-            className="p-1"
-            variant="ghost"
-            disabled={value === ""}
-            onClick={handleMessageSend}
-            onKeyDown={handleMessageSend}
-            colorScheme={value === "" ? "secondary" : "primary"}
+    <div
+      className={classNames(
+        "flex items-center justify-between",
+        isUser && "flex flex-row-reverse",
+      )}
+    >
+      <div
+        className={classNames(
+          "group flex items-center gap-2",
+          isUser && "flex flex-row-reverse",
+        )}
+      >
+        <div
+          className={classNames(
+            "flex gap-2",
+            isUser && "flex flex-row-reverse",
+          )}
+        >
+          <Avatar name={chat.by} size="sm" />
+          <div
+            className={classNames(
+              "flex max-w-[90%] items-center gap-3 rounded-md px-3.5 py-1.5",
+              "bg-secondary-100/70 dark:bg-secondary-800",
+            )}
           >
-            <PaperAirplaneIcon className="size-4 stroke-2" />
-          </Button>
-        </Suffix>
-      </InputGroup>
+            <div>
+              <p className="text-primary-600 dark:text-primary-200 text-xs font-semibold capitalize">
+                {chat.by}
+              </p>
+              <p className="font-medium">
+                {chat.message}
+                <span className="text-secondary-400 dark:text-secondary-600 ml-2 text-[0.7rem]">
+                  {chat.time}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <Button
+          size="fab"
+          title="copy"
+          className="invisible group-hover:visible"
+          onClick={() => copyToClipboard(chat.message)}
+        >
+          <DocumentDuplicateIcon className="size-4 stroke-2" />
+        </Button>
+        <Button
+          size="fab"
+          title="delete"
+          className="invisible group-hover:visible"
+        >
+          <TrashIcon className="size-4 stroke-2" />
+        </Button>
+      </div>
     </div>
   );
 }
--- packages/corp/src/chat-box/chat-box.stories.tsx
+++ packages/corp/src/chat-box/chat-box.stories.tsx
@@ -1,5 +1,12 @@
+import {
+  ArrowDownTrayIcon,
+  PaperAirplaneIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+import { Button, InputField, InputGroup, Suffix } from "@rafty/ui";
 import type { Meta, StoryObj } from "@storybook/react";
-import { ChatBox } from "./ChatBox";
+import { useState } from "react";
+import { ChatBox, type ChatType, SENDER } from "./ChatBox";
 
 const meta: Meta<typeof ChatBox> = {
   title: "Corp / ChatBox",
@@ -8,6 +15,145 @@ const meta: Meta<typeof ChatBox> = {
 export default meta;
 type Story = StoryObj<typeof ChatBox>;
 
+const DATA: ChatType[] = [
+  {
+    id: "1",
+    by: "me",
+    message: "hello",
+    sender: SENDER.user,
+    time: "10:15 PM",
+  },
+  {
+    id: "2",
+    by: "Ram",
+    message: "hello",
+    sender: SENDER.other,
+    time: "10:16 PM",
+  },
+  {
+    id: "3",
+    by: "me",
+    message: "hiii",
+    sender: SENDER.user,
+    time: "10:17 PM",
+  },
+  {
+    id: "4",
+    by: "Ram",
+    message: "how r u",
+    sender: SENDER.other,
+    time: "10:17 PM",
+  },
+  {
+    id: "5",
+    by: "me",
+    message: "dkdkfk fj  de",
+    sender: SENDER.user,
+    time: "10:18 PM",
+  },
+  {
+    id: "6",
+    by: "me",
+    message: "hello",
+    sender: SENDER.user,
+    time: "10:15 PM",
+  },
+  {
+    id: "7",
+    by: "Ram",
+    message: "hello",
+    sender: SENDER.other,
+    time: "10:16 PM",
+  },
+  {
+    id: "8",
+    by: "me",
+    message: "hiii",
+    sender: SENDER.user,
+    time: "10:17 PM",
+  },
+  {
+    id: "9",
+    by: "Ram",
+    message: "how r u",
+    sender: SENDER.other,
+    time: "10:17 PM",
+  },
+  {
+    id: "10",
+    by: "me",
+    message: "dkdkfk fj  de",
+    sender: SENDER.user,
+    time: "10:18 PM",
+  },
+];
+
 export const Default: Story = {
-  render: () => <ChatBox />,
+  render: () => {
+    const [data, setData] = useState(DATA);
+
+    const onDownload = () => {
+      // Convert the data array into a CSV string
+      const csvString = [
+        ["ID", "MESSAGE", "BU", "SENDER", "TIME"], // Specify your headers here
+        ...data.map(({ by, id, message, sender, time }) => [
+          id,
+          message,
+          by,
+          sender,
+          time,
+        ]), // Map your data fields accordingly
+      ]
+        .map((row) => row.join(","))
+        .join("\n");
+
+      // Create a Blob from the CSV string
+      const blob = new Blob([csvString], { type: "text/csv" });
+
+      // Generate a download link and initiate the download
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "download.csv";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    };
+
+    return (
+      <div className="dark:border-secondary-700 border-secondary-300 flex h-[500px] w-full flex-col rounded-lg border">
+        <div className="dark:border-secondary-700 border-secondary-300 flex items-center justify-between border-b px-3.5 py-2.5">
+          <h2 className="text-lg font-semibold">Chat</h2>
+          <div className="flex items-center gap-2">
+            <Button variant="ghost" size="icon" onClick={onDownload}>
+              <ArrowDownTrayIcon className="size-4 stroke-2" />
+            </Button>
+            <Button
+              variant="ghost"
+              colorScheme="error"
+              size="icon"
+              onClick={() => setData([])}
+            >
+              <TrashIcon className="size-4 stroke-2" />
+            </Button>
+          </div>
+        </div>
+        <ChatBox data={data} />
+        <div className="flex items-center gap-2 p-3.5">
+          <InputGroup>
+            <InputField
+              placeholder="Type a message..."
+              className="leading-[1.5rem] placeholder:text-[0.875rem]"
+            />
+            <Suffix className="pointer-events-auto">
+              <Button size="icon" className="p-1" variant="ghost">
+                <PaperAirplaneIcon className="size-4 stroke-2" />
+              </Button>
+            </Suffix>
+          </InputGroup>
+        </div>
+      </div>
+    );
+  },
 };
