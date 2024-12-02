a4390f4ab8c6adb8d8df72ddef06154ab8dbbd37
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-12-02 12:47:46 +0530

    chore: replaced lint staged to nano staged

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "{apps,packages,tools}/**/*.{js,jsx,ts,tsx,json}": [
-    // (files) => `nx affected --target=lint --files=${files.join(",")}`,
-    (files) => `pnpm dlx @biomejs/biome check --apply ${files.join(" ")}`,
-  ],
-};
